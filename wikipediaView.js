0f515da9a907eb9fb15e8497be92d546ce3498aa
Matt <mdark.math@gmail.com>
2016-04-28 14:56:41 -0700

    finally able to navigate JSON

--- wikipediaView.js
+++ wikipediaView.js
@@ -3,13 +3,20 @@
 //--------Construct API
   $( "#submitButton" ).click(function() {
      var entry = document.getElementById('wikiSearch').value;
-     var url = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrsearch=" + entry +  "&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&callback=?";
+     var url =
+      "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrsearch=" + entry +  "&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&indexpageids=1&pageids=&callback=?";
     getWiki(url);
   });
 
   //---------------Call API
   function getWiki(url){
     $.getJSON(url, function(data){
-      console.log(data.query);
+          var pageid = data.query.pageids[0];
+          //       for (var key in data) {
+          //   if (data.query(key)) {
+          //     alert(key + " -> " + p[key]);
+          //   }
+          // }
+        console.log(data.query.pages[pageid].title);
     });
   }
