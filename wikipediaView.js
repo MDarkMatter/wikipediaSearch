0516ea62ac0c169c56f646811c8d435e8cbe3bad
Matt <mdark.math@gmail.com>
2016-04-30 19:24:38 -0700

    switched to $.ajax

--- wikipediaView.js
+++ wikipediaView.js
@@ -1,22 +1,28 @@
+//--------Get Entry From Form
+$("#submitButton").click(function() {
+    var entry = document.getElementById('wikiSearch').value;
+    getWiki(entry);
+});
 
+//---------------Call API
 
-//--------Construct API
-  $( "#submitButton" ).click(function() {
-     var entry = document.getElementById('wikiSearch').value;
-     var url =
-      "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrsearch=" + entry +  "&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&indexpageids=1&pageids=&callback=?";
-    getWiki(url);
-  });
+function getWiki(entry) {
+    $.ajax({
+        url: "https://en.wikipedia.org/w/api.php",
+        jsonp: "callback",
+        dataType: "jsonp",
+        data: {
+            action: "opensearch",
+            search: entry,
+            limit: "10",
+            format: "json"
+        },
+        success: function(data) {
+            for (var i = 0; i < data[1].length; i++) {
+                $('.results').append('<div class"return"><a href="' + data[3][i] + '">' + data[1][i] + "</a></div>");
 
-  //---------------Call API
-  function getWiki(url){
-    $.getJSON(url, function(data){
-          var pageid = data.query.pageids[0];
-          //       for (var key in data) {
-          //   if (data.query(key)) {
-          //     alert(key + " -> " + p[key]);
-          //   }
-          // }
-        console.log(data.query.pages[pageid].title);
+            }
+        }
     });
-  }
+
+}
